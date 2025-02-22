8a94fac2a8acb1f17277804842c31f0a3da8c6c4
atk-titan <gauravmane8451@gmail.com>
2025-02-22 22:37:57 +0530

    ABI file addition and web3 error solving

--- backend/contract.js
+++ backend/contract.js
@@ -1,10 +1,13 @@
 import Web3 from 'web3';
-require('dotenv').config();
-import contractABI from './build/ABI.json'; // adjust path if needed
+import dotenv from 'dotenv';
+import fs from 'fs';
 
+dotenv.config();
+
+const contractABI = JSON.parse(fs.readFileSync('./build/ABI.json', 'utf8'));
 const web3 = new Web3(process.env.WEB3_PROVIDER);
 const contractAddress = process.env.JUDICIAL_DEPOSIT_CONTRACT_ADDRESS;
 
 const judicialDepositContract = new web3.eth.Contract(contractABI, contractAddress);
 
-export default { judicialDepositContract };
+module.exports = { judicialDepositContract };
