f755b187bdb68ef5e115fdaf5150bbb73779b83e
Mukesh Gupta <108881020+mig2002@users.noreply.github.com>
2025-03-11 12:53:38 +0530

    complete transaction

--- backend/routes/moneyTransfer.js
+++ backend/routes/moneyTransfer.js
@@ -69,124 +69,73 @@ money.get('/orderID', verifyJWT, async (req, res) => {
     }
 });
 
+money.post('/addTransaction', verifyJWT, async (req, res) => {
+    try {
+        const authToken = req.headers.authorization;
+        const decoded = jwt.decode(authToken);
+        const role = decoded.role;
+        const id = decoded.id;
+
+        if (role !== "lawyer") {
+            return res.status(401).json({ msg: "You are not authorized to do this operation" });
+        }
+
+        const caseId = req.body.caseId;
+        const amount = req.body.amount;
+
+        if (!caseId || !amount) {
+            return res.status(400).json({ msg: "Missing caseId or amount" });
+        }
+
+        const lawyerDets = await Lawyer.findOne({ userId: id });
+        const user = await User.findOne({ _id: id });
+
+        if (!lawyerDets) return res.status(404).json({ msg: "Lawyer not found" });
+
+        if (!lawyerDets.ongoingCases.includes(caseId.toString())) {
+            return res.status(403).json({ msg: `You are not associated with the case ${caseId}` });
+        }
+
+        const caseDetails = await Case.findOne({ _id: caseId });
+        if (!caseDetails) return res.status(404).json({ msg: "Case not found" });
+
+        if (caseDetails.status !== 'Open') {
+            return res.status(400).json({ msg: "The case is not open" });
+        }
+
+        const txParameters = [
+            caseId, // _caseID
+            caseDetails.judge, // _judgeId
+            caseDetails.courtName, // _courtName
+            caseDetails.parties[0] || "", // _party1
+            caseDetails.parties[1] || "", // _party2
+            user.email || user.name, // _from (lawyer identifier)
+            caseDetails.judge, // _to (assuming money goes to the court)
+            amount, // _amount (actual amount from Razorpay)
+            "", // _contentId (empty for money transactions)
+            true, // _transactionType: true for money transfer
+            Math.floor(Date.now() / 1000) // _date (current timestamp)
+        ];
+
+        const senderAddress = process.env.AUTHORIZED_ACCOUNT;
+        const gasEstimate = await judicialDepositContract.methods
+            .addTransaction(...txParameters)
+            .estimateGas({ from: senderAddress });
+
+        const tx = await judicialDepositContract.methods.addTransaction(...txParameters).send({
+            from: senderAddress,
+            gas: gasEstimate + 10000, // Add buffer
+        });
+
+        res.status(200).json({ msg: "The transaction was successful" });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            msg: "Something went wrong while adding transaction to the blockchain",
+            err
+        });
+    }
+});
+
 module.exports = { money };
-       
-       
-       
-       
-       
-       
-       
-       
-       
-        // console.log(order);
-        // // Blockchain transaction details
-        // const txParameters = [
-        //     caseId,                                       // _caseID
-        //     caseDetails.judge,                            // _judgeId
-        //     caseDetails.courtName,                        // _courtName
-        //     caseDetails.parties[0] || "",                 // _party1
-        //     caseDetails.parties[1] || "",                 // _party2
-        //     user.email || user.name,         // _from (lawyer identifier)
-        //     caseDetails.judge,                            // _to (assuming money goes to the court)
-        //     amount,                                       // _amount (actual amount from Razorpay)
-        //     "",                                           // _contentId (empty for money transactions)
-        //     true,                                         // _transactionType: true for money transfer
-        //     Math.floor(Date.now() / 1000)                 // _date (current timestamp)
-        // ];
-        // console.log(txParameters);
-
-        // // Send transaction to blockchain
-        // const senderAddress = process.env.AUTHORIZED_ACCOUNT;
-        // const gasEstimate = await judicialDepositContract.methods
-        //     .addTransaction(...txParameters)
-        //     .estimateGas({ from: senderAddress });
-
-        // const tx = await judicialDepositContract.methods.addTransaction(...txParameters).send({
-        //     from: senderAddress,
-        //     gas: gasEstimate + 10000, // Add buffer
-        // });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
